43a5f0c595f5625898ce8902fd4047df20779e68
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-18 22:46:31 +0700

    membuat construct dan method load barang

---
+++ application/controllers/BrgRusak.php
@@ -0,0 +1,15 @@
+<?php
+    class BrgRusak extends CI_Controller
+    {
+        function __construct(){
+            parent::__construct;
+            $this->load->model('m_brgRusak');
+        }
+        public function loadBarang(){
+            $data = $this->m_brgRusak->tampilBarang();
+            echo json_encode($data);
+            
+        }
+    }
+    
+?>
