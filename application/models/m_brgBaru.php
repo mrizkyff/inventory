370d1ef60e1972504ee4daa334be25cdde43b2ca
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-19 20:08:13 +0700

    Update m_brgBaru.php
    mengganti query dengan query builder, ganti database dengan daftar_barang

--- application/models/m_brgBaru.php
+++ application/models/m_brgBaru.php
@@ -2,32 +2,30 @@
     class M_brgBaru extends CI_Model
     {
         function barangList(){
-            $hasil=$this->db->query("SELECT * FROM brg_baru ORDER BY tanggal DESC");
-            return $hasil->result();
+            $this->db->order_by('tgl_baru','DESC');
+            $this->db->where('status','Baru');
+            return $this->db->get('daftar_barang')->result();
         }
         public function updateFoto($data,$id){
 			$this->db->where('id',$id);
-			return $this->db->update('brg_baru',$data);
+			return $this->db->update('daftar_barang',$data);
         }
         function saveLog($data){
             return $this->db->insert('log_sistem',$data);
         }
 
         function getBarang($id){
-            $this->db->where('kodeBarang',$id);
-            $hasil = $this->db->get('brg_baru');
+            $this->db->where('id',$id);
+            $hasil = $this->db->get('daftar_barang');
             return $hasil->result();
         }
 
-        function register($data){
-            return $this->db->insert('brg_register',$data);
+        function register($id,$data){
+            $this->db->where('id',$id);
+            return $this->db->update('daftar_barang',$data);
         }
 
-        function delete($id){
-            $this->db->where('id',$id);
-            $hasil = $this->db->delete('brg_baru');
-            return $hasil;
-        }
+        
     }
 
     
