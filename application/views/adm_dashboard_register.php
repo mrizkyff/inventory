f2b3c0b192cc7eb197f99b801608856e2ad6a12f
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-19 08:07:18 +0700

    penambahan modal rusak untuk fitur rusak

--- application/views/adm_dashboard_register.php
+++ application/views/adm_dashboard_register.php
@@ -223,3 +223,27 @@
         </div>
         </div>
         <!-- akhir modal barcode -->
+
+        <!-- modal rusak -->
+        <div class="modal" tabindex="-1" role="dialog" id="modalRusak">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Konfirmasi Rusak</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <center>
+                <p id="textRusak"></p>
+            </center>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Konfirmasi</button>
+            </div>
+            </div>
+        </div>
+        </div>
+        <!-- akhir modal rusak -->
