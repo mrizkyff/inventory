5d53aaccc6126d62dd38e451260f7f261f634366
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-19 21:03:00 +0700

    Update m_brgRegister.php
    membuat query builder dan method rusak

--- application/models/m_brgRegister.php
+++ application/models/m_brgRegister.php
@@ -2,15 +2,20 @@
     class M_brgRegister extends CI_Model
     {
         function listBarang(){
-            return $this->db->get("brg_register")->result();
+            $this->db->where('status','Terdaftar');
+            return $this->db->get('daftar_barang')->result();
         }
 
         function update($data, $id){
             $this->db->where('id',$id);
-            return $this->db->update('brg_register',$data);
+            return $this->db->update('daftar_barang',$data);
         }
         function saveLog($data){
             return $this->db->insert('log_sistem',$data);
+        }
+        function rusak($id, $data){
+            $this->db->where('id',$id);
+            return $this->db->update('daftar_barang',$data);
         }
         
     }
