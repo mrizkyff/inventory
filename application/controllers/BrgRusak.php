fd6217a0509faf0dc2e1de45c6bd07469e92c412
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-20 01:40:06 +0700

    Update BrgRusak.php
    membuat method perbaikan dan upgrade

--- application/controllers/BrgRusak.php
+++ application/controllers/BrgRusak.php
@@ -9,6 +9,69 @@
             $data = $this->m_brgRusak->tampilBarang();
             echo json_encode($data);
         }
+        public function perbaikan(){
+            $tanggal = date("Y-m-d H:i:s");
+            $id = $this->input->post('id');
+            $nama = $this->input->post('nama');
+            $status = 'Terdaftar';
+            $action = $this->input->post('action');
+            $username = $this->input->post('username');
+            $perbaikan = $this->input->post('perbaikan');
+
+            $dt = array(
+                'status' => $status,
+                'perbaikan' => $perbaikan,
+                'tgl_register' => $tanggal
+            );
+
+            $data = $this->m_brgRusak->repair($dt,$id);
+
+            // insert data di log
+            $log = array(
+                'kode_brg' => $id,
+                'nama_brg' => $nama,
+                'edit_by' => $username,
+                'activity' => $action,
+                'date' => $tanggal,
+                'perbaikan' => $perbaikan
+            );
+
+            $data = $this->m_brgRusak->saveLog($log);
+
+            echo json_encode($data);
+
+        }
+        public function upgrade(){
+            $tanggal = date("Y-m-d H:i:s");
+            $id = $this->input->post('id');
+            $nama = $this->input->post('nama');
+            $status = 'Rusak';
+            $action = $this->input->post('action');
+            $username = $this->input->post('username');
+            $upgrade = $this->input->post('upgrade');
+
+            $dt = array(
+                'status' => $status,
+                'upgrade' => $upgrade,
+                'tgl_register' => $tanggal
+            );
+
+            $data = $this->m_brgRusak->upgrade($dt,$id);
+
+            // insert data di log
+            $log = array(
+                'kode_brg' => $id,
+                'nama_brg' => $nama,
+                'edit_by' => $username,
+                'activity' => $action,
+                'date' => $tanggal,
+                'upgrade' => $upgrade
+            );
+
+            $data = $this->m_brgRusak->saveLog($log);
+
+            echo json_encode($data);
+        }
     }
     
 ?>
