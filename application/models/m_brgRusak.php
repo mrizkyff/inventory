1b2929118f5ec73b79327a989bb99504fc1623c9
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-20 01:41:50 +0700

    Update m_brgRusak.php
    query builder, method repair, savelog, upgrade

--- application/models/m_brgRusak.php
+++ application/models/m_brgRusak.php
@@ -2,9 +2,22 @@
     class M_brgRusak extends CI_Model
     {
         function tampilBarang(){
-            $hasil =  $this->db->get('brg_rusak');
+            $this->db->order_by('tgl_rusak','DESC');
+            $this->db->where('status','Rusak');
+            $hasil =  $this->db->get('daftar_barang');
             return $hasil->result();
-        }        
+        }    
+        function repair($data,$id){
+            $this->db->where('id',$id);
+            return $this->db->update('daftar_barang',$data);
+        }    
+        function saveLog($data){
+            return $this->db->insert('log_sistem',$data);
+        }
+        function upgrade($data,$id){
+            $this->db->where('id',$id);
+            return $this->db->update('daftar_barang',$data);
+        }
     }
     
 ?>
