f15c722d184d832e8c845fb02338e2ca81eabf9f
mrizkyff <50722219+mrizkyff@users.noreply.github.com>
2020-02-24 17:46:27 +0700

    Update m_setting.php
    method save dan get layout barcode

--- application/models/m_setting.php
+++ application/models/m_setting.php
@@ -28,6 +28,20 @@
             $this->db->where('id','1');
             return $this->db->update('setting_subbag',$data);
         }
+
+
+        function saveLayout($id, $data, $table){
+            $this->db->where('id',$id);
+            return $this->db->update($table,$data);
+        }
+        function getLayoutQr(){
+            $this->db->where('id','1');
+            return $this->db->get('setting_qr')->result();
+        }
+        function getLayoutBarcode(){
+            $this->db->where('id','1');
+            return $this->db->get('setting_barcode')->result();
+        }
     }
     
 ?>
